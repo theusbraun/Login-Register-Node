ff5100a022b11d76915e337eb50aa04cef025afd
theusbraun <58713336+theusbraun@users.noreply.github.com>
2021-06-25 00:40:12 -0300

    Tabela usuario automatico

--- server/infraestrutura/tabelas.js
+++ server/infraestrutura/tabelas.js
@@ -2,18 +2,18 @@ class Tabelas {
     init(conexao) {
         this.conexao = conexao
 
-        /*this.criarAtendimentos()*/
+        this.criarUsuarios()
     }
 
-    /*criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+    criarUsuarios() {
+        const sql = 'CREATE TABLE IF NOT EXISTS usuarios (id int NOT NULL AUTO_INCREMENT,nome varchar(100) NOT NULL,data_nascimento date NOT NULL,cpf varchar(11) NOT NULL,email varchar(50) NOT NULL,senha varchar(255) DEFAULT NULL,PRIMARY KEY (id))'
 
         this.conexao.query(sql, erro => {
             if(erro) {
                 console.log(erro)
             } 
         })
-    }*/
+    }
 }
 
 module.exports = new Tabelas
